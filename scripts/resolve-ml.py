00147868e1c3f7b3f5e71072cc904ada3d32deeb
STAR WARS <37897885+ariefwindhiarto@users.noreply.github.com>
2025-05-18 02:04:05 +0700

    Update resolve-ml.py

--- scripts/resolve-ml.py
+++ scripts/resolve-ml.py
@@ -1,1 +1,24 @@
+import socket
+from datetime import datetime
 
+with open("domains.txt") as f:
+    domains = [line.strip() for line in f if line.strip()]
+
+ips = set()
+
+for domain in domains:
+    try:
+        results = socket.gethostbyname_ex(domain)[2]
+        for ip in results:
+            ips.add(ip)
+    except:
+        continue
+
+with open("raw/ip-mobilelegends.rsc", "w") as rsc:
+    rsc.write("# Updated on {}\n".format(datetime.now()))
+    for ip in sorted(ips):
+        rsc.write(f"/ip firewall address-list add list=mobilelegends address={ip} comment=auto-ml\n")
+
+with open("raw/ip-mobilelegends.txt", "w") as txt:
+    for ip in sorted(ips):
+        txt.write(f"{ip}\n")
